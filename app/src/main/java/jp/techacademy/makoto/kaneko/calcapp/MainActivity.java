2291d4281c5b847e72822a48adf09f506067db34
Makotovv <ma-kasbs757@chic.ocn.ne.jp>
2018-11-01 22:40:16 +0900

    最新初回課題データ

--- app/src/main/java/jp/techacademy/makoto/kaneko/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/makoto/kaneko/calcapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -30,51 +31,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button buttonwaru = (Button) findViewById(R.id.waru);
         buttonwaru.setOnClickListener(this);
 
-//        editText1 = (EditText) findViewById(R.id.textEdit1);
-//        editText2 = (EditText) findViewById(R.id.textEdit2);
+        editText1 = (EditText) findViewById(R.id.textEdit1);
+        editText2 = (EditText) findViewById(R.id.textEdit2);
     }
 
     @Override
     public void onClick(View v) {
 
-        try{
-            editText1 = (EditText) findViewById(R.id.textEdit1);
-            editText2 = (EditText) findViewById(R.id.textEdit2);
+      try {
+//
 
-        }catch (NumberFormatException e){
-            return;
-        }
+          int hikaku = 0;
+          if (v.getId() == R.id.tasu) {
+              hikaku = 1;
+          } else if (v.getId() == R.id.hiku) {
+              hikaku = 2;
+          } else if (v.getId() == R.id.kakeru) {
+              hikaku = 3;
+          } else if (v.getId() == R.id.waru) {
+              hikaku = 4;
+          }
 
-        int hikaku = 0 ;
-        if(v.getId() == R.id.tasu){
-            hikaku=1;
-        }else if (v.getId() == R.id.hiku){
-            hikaku=2;
-        }else if (v.getId() == R.id.kakeru){
-            hikaku=3;
-        }else if (v.getId() == R.id.waru){
-            hikaku=4;
-        }
-
-        Intent intent = new Intent(this, Main2Activity.class);
+          Intent intent = new Intent(this, Main2Activity.class);
 //        intent.putExtra("VALUE1",editText1.getText().toString());
 //        intent.putExtra("VALUE2",editText2.getText().toString());
 
-        String sss1 = editText1.getText().toString();
-        String sss2 = editText2.getText().toString();
+          String sss1 = editText1.getText().toString();
+          String sss2 = editText2.getText().toString();
 
-        double dd1 = Double.parseDouble(sss1);
-        double dd2 = Double.parseDouble(sss2);
+          double dd1 = Double.parseDouble(sss1);
+          double dd2 = Double.parseDouble(sss2);
 
-        Log.d("javatest", "ログへの出力テストログ１");
-        Log.d("javatest", "dd1=" + dd1);
-        Log.d("javatest", "dd2=" + dd2);
+          Log.d("javatest", "ログへの出力テストログ１");
+          Log.d("javatest", "dd1=" + dd1);
+          Log.d("javatest", "dd2=" + dd2);
 
-        intent.putExtra("hikaku",hikaku);
-        intent.putExtra("VALUE1",dd1);
-        intent.putExtra("VALUE2",dd2);
-        startActivity(intent);
-
+          intent.putExtra("hikaku", hikaku);
+          intent.putExtra("VALUE1", dd1);
+          intent.putExtra("VALUE2", dd2);
+          startActivity(intent);
+      }catch(NumberFormatException e){
+          Toast.makeText(this, "数字を入力してください", Toast.LENGTH_SHORT).show();
+          return;
+      }
     }
 }
 
